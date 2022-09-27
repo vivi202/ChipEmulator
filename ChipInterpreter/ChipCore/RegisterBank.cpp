5b40453e6ccfb788d650dcfd44a95044ce1a5bf4
Vincenzo Marturano <mart.vivi@hotmail.it>
2022-09-27 22:15:55 +0200

    initial commit, set up of project structure.

---
+++ ChipInterpreter/ChipCore/RegisterBank.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vincenzo on 27/09/22.
+//
+
+#include "RegisterBank.h"
---
+++ ChipInterpreter/ChipCore/RegisterBank.h
@@ -0,0 +1,14 @@
+//
+// Created by vincenzo on 27/09/22.
+//
+
+#ifndef CHIPEMULATOR_REGISTERBANK_H
+#define CHIPEMULATOR_REGISTERBANK_H
+
+
+class RegisterBank {
+
+};
+
+
+#endif //CHIPEMULATOR_REGISTERBANK_H
